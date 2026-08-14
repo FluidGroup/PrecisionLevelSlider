de4ddf90e5598798d40cc8cf937902d26a26aadf
Antoine Marandon <antoine@marandon.fr>
2026-08-14 19:38:38 +0900

    Update hosting dependency

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.3
 import PackageDescription
 
 let package = Package(
   name: "PrecisionLevelSlider",
   platforms: [
-    .iOS(.v15)
+    .iOS(.v17)
   ],
   products: [
     .library(name: "PrecisionLevelSlider", targets: ["PrecisionLevelSlider"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/FluidGroup/swiftui-Hosting", from: "2.0.0"),
+    .package(url: "https://github.com/ntnmrndn/swiftui-Hosting", from: "2.0.0"),
   ],
   targets: [
     .target(
